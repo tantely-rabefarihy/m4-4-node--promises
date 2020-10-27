d0c3ff8312a56eea551ad64ff40cd66eb59bd7c3
Tantely Rabefarihy <tantelyrab@gmail.com>
2020-10-27 15:51:48 -0400

    exo3

--- __workshop/exercise-2.3.js
+++ __workshop/exercise-2.3.js
@@ -5,3 +5,24 @@
 const request = require('request-promise');
 
 // get the code you wrote in 2.2 and paste it here...
+const greeting = (langCode) => {
+  
+    return request(`https://journeyedu.herokuapp.com/hello/${langCode}`) // 1
+      .then((response) => JSON.parse(response))
+      .then((parsedResponse) => {
+  
+        return parsedResponse.data; // 2
+      })
+      .catch((err) => {
+return err.error ? JSON.parse(err.error) : err;
+      })
+  };
+  
+  // Testing
+  greeting('fr').then((result) => console.log(result)); // { lang: "French", code: "FR", text: "Bonjour" }
+  
+  // 3
+  greeting('it').then((result) => console.log(result));
+  greeting('es').then((result) => console.log(result));
+  greeting('de').then((result) => console.log(result));
+  greeting('kgn').then((result) => console.log(result));
--- __workshop/exercise-3.1.js
+++ __workshop/exercise-3.1.js
@@ -2,8 +2,23 @@
 // -----------------------------
 
 // require the 'request-promise' module.
+const request = require("request-promise");
+
 
 // Returns the current position of the ISS
 const getIssPosition = () => {
-  // write some code...
+
+  return request("http://api.open-notify.org/iss-now.json")
+  .then((response) => JSON.parse(response))
+  .then((parsedResponse) => {
+    const lat = parsedResponse.iss_position.latitude;
+    const lng = parsedResponse.iss_position.longitude;
+    const position = {lat, lng};
+
+    return position;
+  })
+
+  .catch((err) => console.log("Error: ", err))
 };
+
+getIssPosition().then((result) => console.log(result));
